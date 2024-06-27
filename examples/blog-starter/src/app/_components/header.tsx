c844c2c4925f5537848b484d5ebf26829a44fb39
RVCA212 <112829052+RVCA212@users.noreply.github.com>
2024-06-27 15:17:50 +0200

    Update header.tsx

--- examples/blog-starter/src/app/_components/header.tsx
+++ examples/blog-starter/src/app/_components/header.tsx
@@ -4,11 +4,25 @@ const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
       <Link href="/" className="hover:underline">
-        Blog
+        RAG.pro
       </Link>
-      .
     </h2>
   );
 };
 
 export default Header;
+
+// import Link from "next/link";
+
+// const Header = () => {
+//   return (
+//     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
+//       <Link href="/" className="hover:underline">
+//         Blog
+//       </Link>
+//       .
+//     </h2>
+//   );
+// };
+
+// export default Header;
